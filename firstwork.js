dd60b0e224a259c075654ee8946ddeb310958ec7
Chinta Joshua <joshuachinta@gmail.com>
2025-04-05 22:34:05 +0530

    Update firstwork.js

--- firstwork.js
+++ firstwork.js
@@ -1,1 +1,48 @@
+//Variables
+let name = "Joshua";     
+let age = 20; 
+console.log(name);
+console.log(age);
+// String
+let city = "Delhi";
+console.log(city);
+// Number
+let temperature = 32.5;
+console.log(temperature);
+// Boolean
+let isSunny = true;
+console.log(isSunny);
+// Object
+let person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 25
+};
+console.log(firstName);
+// Array
+let colors = ["red", "green", "blue"];
+console.log(colors);
+//Opreators
+let a = 10;
+let b = 3;
+console.log(a + b); 
+console.log(a * b);
+//Assignment operators
+console.log(5 == '5');   // true (loose equality)
+console.log(5 === '5');  // false (strict equality)
+console.log(5 != 3);     // true
+console.log(5 > 3);      // true
+console.log(5 < 3);      // false
+// Function with no parameters
+function greet() {
+  console.log("Hello, welcome!");
+}
+
+greet(); 
+//Arrays
+let fruits = ["apple", "banana", "mango"];
+
+// Accessing elements
+console.log(fruits[0]);  
+
 
